f66976d74d5a7e9c1aebeedd2674ef1bc1a2cffa
02rasjac <rasmusjacklin2002@gmail.com>
2021-09-27 14:43:35 +0200

    Add Sprite for winning

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -26,4 +26,5 @@ public:
 	static void DrawTileFlag( const Vei2& pos,Graphics& gfx );
 	static void DrawTileBomb( const Vei2& pos,Graphics& gfx );
 	static void DrawTileBombRed( const Vei2& pos,Graphics& gfx );
+	static void DrawWin( const Vei2& pos,Graphics& gfx );
 };
